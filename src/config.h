114044fd3dda6887f8c8e8905474e2c97553913c
veeloth <qweeloth@gmail.com>
2025-04-19 01:16:47 -0300

    first commit

---
+++ src/config.h
@@ -0,0 +1,11 @@
+/* This file is an example. You must configure this yourself. */
+
+bool debug_print_spawned_command = false;
+
+struct MouseEvent mouse_events[] = {
+};
+struct KeyEvent key_events[] = {
+
+    /* Applications */
+    { KeyPress, Mod4Mask, XK_o, "scrot -s -f -o \"/tmp/image.png\" && xclip -selection clipboard -t image/png -i /tmp/image.png" },
+};
